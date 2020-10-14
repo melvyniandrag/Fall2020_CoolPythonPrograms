0826cb9aeae09cb941b00193624bee34bb3b0261
Jefferson Hilaire <hilairejeff3@gmail.com>
2020-10-14 01:44:13 +0000

    added pythonprogram.py in my directory

---
+++ JeffersonHilaire/pythonprogram.py
@@ -0,0 +1,22 @@
+# Python program to convert decimal into other number systems
+dec = 81
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
